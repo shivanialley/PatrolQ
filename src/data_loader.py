5320f03f35dfbd429a0f90d1953eca0f161602fe
shivani.alley <shivani.alley@gmail.com>
2026-01-22 18:58:44 +0530

    Remove all existing files

--- src/data_loader.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def load_data(path):
-    df = pd.read_csv(r"C:\Users\Dell\Documents\guvi\Project\PatrolQ\data\Crimes_-_2001_to_Present_20251215.csv")
-    return df
--- src/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def get_logger():
-    logging.basicConfig(
-        filename="logs/pipeline.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-    return logging.getLogger()
